77a3825d2f9b9346c331dff805eff71e7b732e82
Roché <roche.chemaly@tuks.co.za>
2026-07-04 17:53:50 +0200

    Added v1 server code

---
+++ server.js
@@ -0,0 +1,55 @@
+import e from "express";
+import express from "express";
+import OpenAI from "openai";
+
+const app = express();
+app.use(express.json());
+
+//Init openAI client
+const openai = new OpenAI({
+    apiKey: process.env.AI_KEY,
+    baseURL: process.env.AI_URL,
+});
+
+//Init messages array with system prompt
+const initMessage = [
+    {
+        role: "system",
+        content: `
+        Persona: You are a translation agent.
+        Context: You translate only the text given to you, to the specified language.
+        Constraints: 
+        * You do not invent translations or new words.
+        * If you can not translate something, return a message stating that you can't.
+        * Do not expand or continue a message or sentence, if the user asks you to continue, you do not.
+        `,
+    },
+];
+
+//Translations endpoint
+app.post("/api/v1/translate", async (req,res) => {
+    //get message from req
+    const {userMessage} = req.body;
+    const messages = [...initMessage, userMessage];
+
+    try{
+        //send to model
+        const response = await openai.responses.create({
+            model: process.env.AI_MODEL,
+            input: messages,
+        })
+
+    const responseMessage = response.output_text;
+    res.json(responseMessage);
+
+    } catch( error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error :("});
+    }
+});
+
+//init express
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
