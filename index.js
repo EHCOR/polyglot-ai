42d5f1f2a6958d96a44ac8982e57172aa6ae32ae
Roché <roche.chemaly@tuks.co.za>
2026-07-04 18:34:56 +0200

    add basic ui features

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 //front
 import {marked} from "marked";
 import DOMPurify from "dompurify";
+
+
+//get UI elems
+const submitBtn = document.getElementById("user-submit-btn");
+const userInputText = document.getElementById("user-input-text");
+
+function start() {
+    submitBtn.addEventListener("click", handleSubmit);
+}
+
+async function handleSubmit(e) {
+    e.preventDefault();
+
+    const userPrompt = userInputText.value.trim();
+    
+    
+}
+
+
+start();
